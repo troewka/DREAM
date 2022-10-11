4fef9b304d72757137542755c985684f9896c43c
troewka <troewka9@gmail.com>
2022-10-11 11:57:56 +0300

    udemy lessons #1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-let numbers = 10;
+const numberOfFilms = +prompt("Скільки фільмів ви вже подивились?", "");
+
+const personMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
+
+const a = prompt("Один із останніх переглянутих фільмів?", "");
+const b = prompt("На скільки оціните його?", "");
+const c = prompt("Один із останніх переглянутих фільмів?", "");
+const d = prompt("На скільки оціните його?", "");
+
+personMovieDB.movies[a] = b;
+personMovieDB.movies[c] = d;
+
+console.log(personMovieDB);
